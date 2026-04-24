fee04856582adc2e4d548acce04d5e45c0996d23
Christopher Atwell <catwell@mys3tech.com>
2026-04-24 08:35:27 -0400

    Initial commit

---
+++ src/client/java/com/regionsmod/client/RegionsModClient.java
@@ -0,0 +1,9 @@
+package com.regionsmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RegionsModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
